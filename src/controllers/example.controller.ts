982cc32cae915ea1de38cc757c7974db987b8c50
HigorSkw <higor.skowronski@gmail.com>
2022-11-11 23:32:38 -0300

    feat: setting folders and files

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
+import salesUploadService from "../services/example.service";
 
-const myController = async (req: Request, res: Response) => {
-  console.log("Exemplo de controoler que irei utilizar!");
+const salesUploadController = async (req: Request, res: Response) => {
+  const { file } = req;
+  const buffer = file?.buffer;
+
+  const salesUploadComplete = await salesUploadService(buffer);
+
+  return res.status(200).json(salesUploadComplete);
 };
 
-export default myController;
+export default salesUploadController;
